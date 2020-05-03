5e1e98f980e95b490445081e1a43548bb5e07639
Kevin Pageau <pageau.kevin@gmail.com>
2020-05-03 13:45:05 -0400

    Change CPU update func to use `top` command

--- addon/cpu.go
+++ addon/cpu.go
@@ -3,29 +3,35 @@ package addon
 import (
 	"fmt"
 	"github.com/lsgrep/gostatus/log"
-
+	"os/exec"
+	"strings"
 	"time"
-
-	linuxproc "github.com/c9s/goprocinfo/linux"
 )
+
+const cpuCommand = "top -bn1 | sed -n '/Cpu/p'"
 
 type cpu struct {
 }
 
 func (c *cpu) Update() *Block {
-	stat, err := linuxproc.ReadStat("/proc/stat")
+	out, err := exec.Command("bash", "-c", cpuCommand).Output();
+
 	if err != nil {
 		log.Error(err)
 		return nil
 	}
 
-	cpu := stat.CPUStatAll
-	cpuUsage := cpu.System + cpu.User + cpu.Nice + cpu.Guest + cpu.GuestNice + cpu.IOWait + cpu.IRQ + cpu.SoftIRQ + cpu.Steal
-	usage := float64(cpuUsage*100) / float64(cpuUsage+cpu.Idle)
+	// Extract percentage from command output
+	pStart := strings.Index(string(out), ":") + 1
+	pEnd := strings.Index(string(out), " us")
 
-	fullTxt := fmt.Sprintf(" %s  %s", IconCPU, fmt.Sprintf("%.2f%%", usage))
-	b := &Block{FullText: fullTxt}
-	return b
+	formattedOut := strings.TrimSpace(string(out)[pStart: pEnd])
+
+	// Convert to a float value so we can change output color based on how high it is
+	// TODO
+	// floatVal, _ := strconv.ParseFloat(formattedOut, 16)
+
+	return &Block{FullText: fmt.Sprintf("%s %s%%", IconCPU, formattedOut)}
 }
 
 func NewCPUAddon() *Addon {
