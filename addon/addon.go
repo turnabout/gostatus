85518d95dbc500959debca7ee905fc3963638e80
Kevin Pageau <pageau.kevin@gmail.com>
2020-05-15 09:18:29 -0400

    Clean up some commented code

--- addon/addon.go
+++ addon/addon.go
@@ -4,26 +4,3 @@ type Addon interface {
 	Run(ch chan *Block)
 	getBlock() *Block
 }
-
-
-/*
-type Addon struct {
-	UpdateInterval time.Duration
-	Updater        Updater
-
-	//Icon           string
-	//Run            AddonRunner
-}
- */
-
-// Runs the Addon continuously, at its given interval
-/*
-func (a *Addon) Run(ch chan *Block) {
-
-	for range time.Tick(a.UpdateInterval) {
-		// generating data should not be locked
-		newData := a.Updater.Update()
-		a.NewData = newData
-	}
-}
- */
