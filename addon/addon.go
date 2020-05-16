b71e3f6586f6288fb908cb6053721301b60e8331
Kevin Pageau <pageau.kevin@gmail.com>
2020-05-16 18:27:18 -0400

    Add comments

--- addon/addon.go
+++ addon/addon.go
@@ -1,6 +1,16 @@
 package addon
 
+// Interface that wraps all addons
+//
+// Run contains the logic that produces new Blocks over time.
+// When it updates is dependant on the addon's own logic.
+// blocks and blocksRendered are the channels where the Addon must send new Blocks.
+// Blocks sent to blocks will be passed to the status bar, but will not instantly make it render.
+// Blocks sent to blocksRendered will be passed to the status bar, and will make it render
+// immediately upon reception.
+//
+// getBlock returns a new, updated block.
 type Addon interface {
-	Run(ch chan *Block)
+	Run(blocks chan *Block, blocksRendered chan *Block)
 	getBlock() *Block
 }
